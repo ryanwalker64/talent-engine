90445f026e4a35c4620514bc961321130524edca
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-08-26 10:25:39 +1200

    create-profile ready

--- scripts/create-profile.js
+++ scripts/create-profile.js
@@ -289,7 +289,7 @@ function nextPrev(n) {
   // This function will figure out which tab to display
   let x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false; 
+//   if (n == 1 && !validateForm()) return false; 
   // Hide the current tab:
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
