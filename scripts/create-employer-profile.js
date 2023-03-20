32b9a512cd07fbfa3f93b847958fcb389161f0fb
ryanwalker64 <ryan.walkerz46@gmail.com>
2023-03-21 09:40:01 +1300

    fix

--- scripts/create-employer-profile.js
+++ scripts/create-employer-profile.js
@@ -1,5 +1,5 @@
 const API = "https://v1.nocodeapi.com/startmate/airtable/fVDPLsNPEAUNPlBG?tableName="
-const JSDELIVR = 'https://cdn.jsdelivr.net/gh/ryanwalker64/talent-engine@v1.3.9.4/'
+const JSDELIVR = 'https://cdn.jsdelivr.net/gh/ryanwalker64/talent-engine@v1.3.9.6/'
 
 let userProfile = {}
 let userId
